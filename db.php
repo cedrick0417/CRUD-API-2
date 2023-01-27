37d862b800bfb1f1b8cb092d9cca182fa2ff67e9
Cedrick Garcia <cedrick.garcia@ippphilippines.com>
2023-01-27 16:29:24 +0800

    Add sql statements for crud operartions

--- db.php
+++ db.php
@@ -12,7 +12,34 @@ class Database extends Config
         }
         $stmt = $this->conn->prepare($sql);
         $stmt->execute(['id' => $id]);
-	    $rows = $stmt->fetchAll();
-	    return $rows;
+        $rows = $stmt->fetchAll();
+        return $rows;
+    }
+
+    // Insert an user in the database
+    public function insert($name, $email, $phone)
+    {
+        $sql = 'INSERT INTO users (name, email, phone) VALUES (:name, :email, :phone)';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(['name' => $name, 'email' => $email, 'phone' => $phone]);
+        return true;
+    }
+
+    // Update an user in the database
+    public function update($name, $email, $phone, $id)
+    {
+        $sql = 'UPDATE users SET name = :name, email = :email, phone = :phone WHERE id = :id';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(['name' => $name, 'email' => $email, 'phone' => $phone, 'id' => $id]);
+        return true;
+    }
+
+    // Delete an user from database
+    public function delete($id)
+    {
+        $sql = 'DELETE FROM users WHERE id = :id';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return true;
     }
 }
